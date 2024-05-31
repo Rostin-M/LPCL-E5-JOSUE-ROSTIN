78992083bfc29cff73d19de2ab053c68cb2f2012
Rostin-M <126520228+Rostin-M@users.noreply.github.com>
2024-05-31 05:27:06 -0500

    ㅤ
    ㅤ

--- E5/app/controllers/pension_controller.py
+++
@@ -1,5 +0,0 @@
-from app.models.pension_calculator import Calcular
-
-def calcular_pension(edad_actual, sexo, salario_actual, semanas_laboradas, ahorro_actual, rentabilidad_fondo, tasa_administracion):
-    calculo = Calcular(edad_actual, sexo, salario_actual, semanas_laboradas, ahorro_actual, rentabilidad_fondo, tasa_administracion)
-    return calculo.calcular_pension()
--- E5/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('0011')
